84b6c2e9b659198f7f6a69bbe5f905ef99d0d2d5
Hyouhyan <hyouhyan1201@gmail.com>
2024-01-15 18:40:02 +0900

    xlsx2csvの作成

--- pdf2csv.py
+++ pdf2csv.py
@@ -1,8 +1,6 @@
 from tabula import read_pdf
-import csv
 
-df = read_pdf("愛知工業大学 1月出店スケジュール.pdf", pages="all")
-export_data = df[0]
-print(export_data)
-
-export_data.to_csv("output.csv", index=False)
+def pdf2csv(pdf_path, csv_path):
+    df = read_pdf(pdf_path, pages="all")
+    export_data = df[0]
+    export_data.to_csv(csv_path, index=False)
--- xlsx2csv.py
+++ xlsx2csv.py
@@ -1,1 +1,6 @@
-import 
+import pandas as pd
+# require openpyxl
+
+def xlsx2csv(xlsx_file, csv_file):
+    data_xlsx = pd.read_excel(xlsx_file)
+    data_xlsx.to_csv(csv_file, encoding='utf-8', index = None, header=True)
