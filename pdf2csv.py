77e4690229377c40ea3eea1f2b1679010f831ef3
Hyouhyan <hyouhyan1201@gmail.com>
2024-01-15 19:52:05 +0900

    エラー対策

--- pdf2csv.py
+++ pdf2csv.py
@@ -1,6 +1,22 @@
 from tabula import read_pdf
+import os
 
 def pdf2csv(pdf_path, csv_path):
+    # pdf_pathが指定されていない場合は、処理終了
+    if pdf_path == '':
+        print('pdfファイルを指定してください')
+        return
+    
+    # ファイル名がpdfで終わるか確認
+    if os.path.splitext(pdf_path)[1] != '.pdf':
+        print('pdfファイルを指定してください')
+        return
+    
+    # csv_pathが指定されていない場合は、pdf_pathと同じ場所にcsvファイルを作成
+    if csv_path == '':
+        csv_path = os.path.splitext(pdf_path)[0] + '.csv'
+
+    # pdfファイルを読み込み
     df = read_pdf(pdf_path, pages="all")
     export_data = df[0]
     export_data.to_csv(csv_path, index=False)
--- xlsx2csv.py
+++ xlsx2csv.py
@@ -1,6 +1,22 @@
 import pandas as pd
 # require openpyxl
+import os
 
 def xlsx2csv(xlsx_file, csv_file):
+    # xlsx_fileが指定されていない場合は、処理終了
+    if xlsx_file == '':
+        print('xlsxファイルを指定してください')
+        return
+
+    # ファイル名がxlsxで終わるか確認
+    if os.path.splitext(xlsx_file)[1] != '.xlsx':
+        print('xlsxファイルを指定してください')
+        return
+    
+    # csv_fileが指定されていない場合は、xlsx_fileと同じ場所にcsvファイルを作成
+    if csv_file == '':
+        csv_file = os.path.splitext(xlsx_file)[0] + '.csv'
+    
+    # xlsxファイルを読み込み
     data_xlsx = pd.read_excel(xlsx_file)
     data_xlsx.to_csv(csv_file, encoding='utf-8', index = None, header=True)
