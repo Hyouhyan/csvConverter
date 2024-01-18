362e975b1210980e8637afd4de58b067e43b582e
Hyouhyan <hyouhyan@hyouhyan.com>
2024-01-18 23:05:18 +0900

    Update xlsx2csv.py

--- xlsx2csv.py
+++ xlsx2csv.py
@@ -19,4 +19,4 @@ def xlsx2csv(xlsx_file, csv_file = ""):
     
     # xlsxファイルを読み込み
     data_xlsx = pd.read_excel(xlsx_file)
-    data_xlsx.to_csv(csv_file, encoding='utf-8', index = None, header=True)
+    data_xlsx.to_csv(csv_file, encoding='utf-8', index = None)
